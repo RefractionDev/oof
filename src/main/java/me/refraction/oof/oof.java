dbe79b999a06dd7a35089a86d02dea535db9b7b3
RefractionDev <callum.calofduty@gmail.com>
2017-11-20 15:31:43 +0000

    Fix for BedWars final kills, fixed chat formatting and general optimisation

--- src/main/java/me/refraction/oof/oof.java
+++ src/main/java/me/refraction/oof/oof.java
@@ -17,7 +17,9 @@ import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import tv.twitch.chat.Chat;
 
 import java.io.*;
@@ -27,7 +29,7 @@ import java.util.ArrayList;
 public class oof
 {
     public static final String MODID = "refractionoof";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static final String NAME = "oof";
 
     private String nickData;
@@ -45,38 +47,17 @@ public class oof
     public void onChatReceived(ClientChatReceivedEvent e) {
         msg = e.message.getUnformattedText();
 
-        // checks if msg is nothing, if yes display it (for some reason it wasn't getting displayed without this??)
-        if(msg.trim().length() == 0 || msg.equals("") || msg == null) {
-            Minecraft.getMinecraft().thePlayer.addChatMessage((IChatComponent)new ChatComponentText(" "));
-        }
-
-        // checks if msg is a divider (e.g. -----------), if yes display it (also wasn't getting displayed without this???)
-        // also this returns the divider in green (&a) always...might wanna change that if I figure out how
-        boolean div = true;
-        char c1 = msg.charAt(0);
-        if(c1 != '-') {
-            div = false;
-        }
-        for(int i = 1; i < msg.length(); i++) {
-            char temp = msg.charAt(i);
-            if(c1 != temp) {
-                div = false;
-            }
-        }
-        if(div) {
-            Minecraft.getMinecraft().thePlayer.addChatMessage((IChatComponent)new ChatComponentText(EnumChatFormatting.GREEN + msg));
-        }
-
         ArrayList<String> formattedDeathMessages = new ArrayList<String>();
         ArrayList<String> nickedFormattedDeathMessages = new ArrayList<String>();
-         for(String m : deathMessages) {
-             formattedDeathMessages.add(m.replace("PLAYER", Minecraft.getMinecraft().thePlayer.getName()));
-         }
-         if(getNick() != "null") {
-             for(String m : deathMessages) {
-                 nickedFormattedDeathMessages.add(m.replace("PLAYER", getNick()));
-             }
-         }
+
+        for(String m : deathMessages) {
+            formattedDeathMessages.add(m.replace("PLAYER", Minecraft.getMinecraft().thePlayer.getName()));
+        }
+        if(getNick() != "null") {
+            for(String m : deathMessages) {
+                nickedFormattedDeathMessages.add(m.replace("PLAYER", getNick()));
+            }
+        }
 
          String[] compare = msg.split(" ", 2);
          // check below if death message is like "was shot by PLAYER (XX)"
@@ -84,16 +65,19 @@ public class oof
 //         if(compare[1].matches("was shot by " + Minecraft.getMinecraft().thePlayer.getName() +" (.*)") || compare[1].matches(".*(.*).")) {
 //             isSnipe = true;
 //         }
-         for(String m : formattedDeathMessages) {
-             if(compare[1].equals(m)) {
-                 Minecraft.getMinecraft().thePlayer.playSound("refractionoof:oof", 5.0f, 1);
-             }
-         }
 
-        if(getNick() != "null") {
-            for(String m : nickedFormattedDeathMessages) {
+        if(compare.length > 1) {
+            for(String m : formattedDeathMessages) {
                 if(compare[1].equals(m)) {
                     Minecraft.getMinecraft().thePlayer.playSound("refractionoof:oof", 5.0f, 1);
+                }
+            }
+
+            if(getNick() != "null") {
+                for(String m : nickedFormattedDeathMessages) {
+                    if(compare[1].equals(m)) {
+                        Minecraft.getMinecraft().thePlayer.playSound("refractionoof:oof", 5.0f, 1);
+                    }
                 }
             }
         }
@@ -111,115 +95,225 @@ public class oof
 
     public void populateDeathMessages() {
         deathMessages.add("was slain by PLAYER");
+        deathMessages.add("was slain by PLAYER FINAL KILL!");
         deathMessages.add("was shot by PLAYER");
+        deathMessages.add("was shot by PLAYER FINAL KILL!");
         deathMessages.add("was thrown into the void by PLAYER");
+        deathMessages.add("was thrown into the void by PLAYER FINAL KILL!");
         deathMessages.add("was doomed to fall by PLAYER");
+        deathMessages.add("was doomed to fall by PLAYER FINAL KILL!");
         deathMessages.add("was toasted by PLAYER");
+        deathMessages.add("was toasted by PLAYER FINAL KILL!");
         deathMessages.add("was killed by PLAYER");
+        deathMessages.add("was killed by PLAYER FINAL KILL!");
         deathMessages.add("was bomberman'd by PLAYER");
+        deathMessages.add("was bomberman'd by PLAYER FINAL KILL!");
         deathMessages.add("was Bomberman'd by PLAYER");
+        deathMessages.add("was Bomberman'd by PLAYER FINAL KILL!");
         deathMessages.add("was thrown off a cliff by PLAYER");
+        deathMessages.add("was thrown off a cliff by PLAYER FINAL KILL!");
         deathMessages.add("was shot and killed by PLAYER");
+        deathMessages.add("was shot and killed by PLAYER FINAL KILL!");
         deathMessages.add("was snowballed to death by PLAYER");
+        deathMessages.add("was snowballed to death by PLAYER FINAL KILL!");
         deathMessages.add("was killed with a potion by PLAYER");
+        deathMessages.add("was killed with a potion by PLAYER FINAL KILL!");
         deathMessages.add("was killed with an explosion by PLAYER");
+        deathMessages.add("was killed with an explosion by PLAYER FINAL KILL!");
         deathMessages.add("was killed with magic by PLAYER");
+        deathMessages.add("was killed with magic by PLAYER FINAL KILL!");
         deathMessages.add("was filled full of lead by PLAYER");
+        deathMessages.add("was filled full of lead by PLAYER FINAL KILL!");
         deathMessages.add("was iced by PLAYER");
+        deathMessages.add("was iced by PLAYER FINAL KILL!");
         deathMessages.add("met their end by PLAYER");
+        deathMessages.add("met their end by PLAYER FINAL KILL!");
         deathMessages.add("lost a drinking contest with PLAYER");
+        deathMessages.add("lost a drinking contest with PLAYER FINAL KILL!");
         deathMessages.add("was killed with dynamite by PLAYER");
+        deathMessages.add("was killed with dynamite by PLAYER FINAL KILL!");
         deathMessages.add("lost the draw to PLAYER");
+        deathMessages.add("lost the draw to PLAYER FINAL KILL!");
         deathMessages.add("was struck down by PLAYER");
+        deathMessages.add("was struck down by PLAYER FINAL KILL!");
         deathMessages.add("was turned to dust by PLAYER");
+        deathMessages.add("was turned to dust by PLAYER FINAL KILL!");
         deathMessages.add("was turned to ash by PLAYER");
+        deathMessages.add("was turned to ash by PLAYER FINAL KILL!");
         deathMessages.add("was melted by PLAYER");
+        deathMessages.add("was melted by PLAYER FINAL KILL!");
         deathMessages.add("was incinerated by PLAYER");
+        deathMessages.add("was incinerated by PLAYER FINAL KILL!");
         deathMessages.add("was vaporized by PLAYER");
+        deathMessages.add("was vaporized by PLAYER FINAL KILL!");
         deathMessages.add("was struck with Cupid's arrow by PLAYER");
+        deathMessages.add("was struck with Cupid's arrow by PLAYER FINAL KILL!");
         deathMessages.add("was given the cold shoulder by PLAYER");
+        deathMessages.add("was given the cold shoulder by PLAYER FINAL KILL!");
         deathMessages.add("was hugged too hard by PLAYER");
+        deathMessages.add("was hugged too hard by PLAYER FINAL KILL!");
         deathMessages.add("drank a love potion from PLAYER");
+        deathMessages.add("drank a love potion from PLAYER FINAL KILL!");
         deathMessages.add("was hit by a love bomb from PLAYER");
+        deathMessages.add("was hit by a love bomb from PLAYER FINAL KILL!");
         deathMessages.add("was no match for PLAYER");
+        deathMessages.add("was no match for PLAYER FINAL KILL!");
         deathMessages.add("was smote from afar by PLAYER");
+        deathMessages.add("was smote from afar by PLAYER FINAL KILL!");
         deathMessages.add("was justly ended by PLAYER");
+        deathMessages.add("was justly ended by PLAYER FINAL KILL!");
         deathMessages.add("was purified by PLAYER");
+        deathMessages.add("was purified by PLAYER FINAL KILL!");
         deathMessages.add("was killed with holy water by PLAYER");
+        deathMessages.add("was killed with holy water by PLAYER FINAL KILL!");
         deathMessages.add("was dealt vengeful justice by PLAYER");
+        deathMessages.add("was dealt vengeful justice by PLAYER FINAL KILL!");
         deathMessages.add("was returned to dust by PLAYER");
+        deathMessages.add("was returned to dust by PLAYER FINAL KILL!");
         deathMessages.add("be shot and killed by PLAYER");
+        deathMessages.add("be shot and killed by PLAYER FINAL KILL!");
         deathMessages.add("be snowballed to death by PLAYER");
+        deathMessages.add("be snowballed to death by PLAYER FINAL KILL!");
         deathMessages.add("be sent to Davy Jones' locker by PLAYER");
+        deathMessages.add("be sent to Davy Jones' locker by PLAYER FINAL KILL!");
         deathMessages.add("be killed with rum by PLAYER");
+        deathMessages.add("be killed with rum by PLAYER FINAL KILL!");
         deathMessages.add("be shot with cannon by PLAYER");
+        deathMessages.add("be shot with cannon by PLAYER FINAL KILL!");
         deathMessages.add("be killed with magic by PLAYER");
+        deathMessages.add("be killed with magic by PLAYER FINAL KILL!");
         deathMessages.add("was glazed in BBQ sauce by PLAYER");
+        deathMessages.add("was glazed in BBQ sauce by PLAYER FINAL KILL!");
         deathMessages.add("was sprinkled with chilli powder by PLAYER");
+        deathMessages.add("was sprinkled with chilli powder by PLAYER FINAL KILL!");
         deathMessages.add("was sprinkled with chili powder by PLAYER");
+        deathMessages.add("was sprinkled with chili powder by PLAYER FINAL KILL!");
         deathMessages.add("was sliced up by PLAYER");
+        deathMessages.add("was sliced up by PLAYER FINAL KILL!");
         deathMessages.add("was overcooked by PLAYER");
+        deathMessages.add("was overcooked by PLAYER FINAL KILL!");
         deathMessages.add("was deep fried by PLAYER");
+        deathMessages.add("was deep fried by PLAYER FINAL KILL!");
         deathMessages.add("was boiled by PLAYER");
+        deathMessages.add("was boiled by PLAYER FINAL KILL!");
         deathMessages.add("was spooked by PLAYER");
+        deathMessages.add("was spooked by PLAYER FINAL KILL!");
         deathMessages.add("was spooked off the map by PLAYER");
+        deathMessages.add("was spooked off the map by PLAYER FINAL KILL!");
         deathMessages.add("was totally spooked by PLAYER");
+        deathMessages.add("was totally spooked by PLAYER FINAL KILL!");
         deathMessages.add("was remotely spooked by PLAYER");
+        deathMessages.add("was remotely spooked by PLAYER FINAL KILL!");
         deathMessages.add("was slain by PLAYER.");
+        deathMessages.add("was slain by PLAYER. FINAL KILL!");
         deathMessages.add("was shot by PLAYER.");
+        deathMessages.add("was shot by PLAYER. FINAL KILL!");
         deathMessages.add("was thrown into the void by PLAYER.");
+        deathMessages.add("was thrown into the void by PLAYER. FINAL KILL!");
         deathMessages.add("was doomed to fall by PLAYER.");
+        deathMessages.add("was doomed to fall by PLAYER. FINAL KILL!");
         deathMessages.add("was toasted by PLAYER.");
+        deathMessages.add("was toasted by PLAYER. FINAL KILL!");
         deathMessages.add("was killed by PLAYER.");
+        deathMessages.add("was killed by PLAYER. FINAL KILL!");
         deathMessages.add("was bomberman'd by PLAYER.");
+        deathMessages.add("was bomberman'd by PLAYER. FINAL KILL!");
         deathMessages.add("was Bomberman'd by PLAYER.");
+        deathMessages.add("was Bomberman'd by PLAYER. FINAL KILL!");
         deathMessages.add("was thrown off a cliff by PLAYER.");
+        deathMessages.add("was thrown off a cliff by PLAYER. FINAL KILL!");
         deathMessages.add("was shot and killed by PLAYER.");
+        deathMessages.add("was shot and killed by PLAYER. FINAL KILL!");
         deathMessages.add("was snowballed to death by PLAYER.");
+        deathMessages.add("was snowballed to death by PLAYER. FINAL KILL!");
         deathMessages.add("was killed with a potion by PLAYER.");
+        deathMessages.add("was killed with a potion by PLAYER. FINAL KILL!");
         deathMessages.add("was killed with an explosion by PLAYER.");
+        deathMessages.add("was killed with an explosion by PLAYER. FINAL KILL!");
         deathMessages.add("was killed with magic by PLAYER.");
+        deathMessages.add("was killed with magic by PLAYER. FINAL KILL!");
         deathMessages.add("was filled full of lead by PLAYER.");
+        deathMessages.add("was filled full of lead by PLAYER. FINAL KILL!");
         deathMessages.add("was iced by PLAYER.");
+        deathMessages.add("was iced by PLAYER. FINAL KILL!");
         deathMessages.add("met their end by PLAYER.");
+        deathMessages.add("met their end by PLAYER. FINAL KILL!");
         deathMessages.add("lost a drinking contest with PLAYER.");
+        deathMessages.add("lost a drinking contest with PLAYER. FINAL KILL!");
         deathMessages.add("was killed with dynamite by PLAYER.");
+        deathMessages.add("was killed with dynamite by PLAYER. FINAL KILL!");
         deathMessages.add("lost the draw to PLAYER.");
+        deathMessages.add("lost the draw to PLAYER. FINAL KILL!");
         deathMessages.add("was struck down by PLAYER.");
+        deathMessages.add("was struck down by PLAYER. FINAL KILL!");
         deathMessages.add("was turned to dust by PLAYER.");
+        deathMessages.add("was turned to dust by PLAYER. FINAL KILL!");
         deathMessages.add("was turned to ash by PLAYER.");
+        deathMessages.add("was turned to ash by PLAYER. FINAL KILL!");
         deathMessages.add("was melted by PLAYER.");
+        deathMessages.add("was melted by PLAYER. FINAL KILL!");
         deathMessages.add("was incinerated by PLAYER.");
+        deathMessages.add("was incinerated by PLAYER. FINAL KILL!");
         deathMessages.add("was vaporized by PLAYER.");
+        deathMessages.add("was vaporized by PLAYER. FINAL KILL!");
         deathMessages.add("was struck with Cupid's arrow by PLAYER.");
+        deathMessages.add("was struck with Cupid's arrow by PLAYER. FINAL KILL!");
         deathMessages.add("was given the cold shoulder by PLAYER.");
+        deathMessages.add("was given the cold shoulder by PLAYER. FINAL KILL!");
         deathMessages.add("was hugged too hard by PLAYER.");
+        deathMessages.add("was hugged too hard by PLAYER. FINAL KILL!");
         deathMessages.add("drank a love potion from PLAYER.");
+        deathMessages.add("drank a love potion from PLAYER. FINAL KILL!");
         deathMessages.add("was hit by a love bomb from PLAYER.");
+        deathMessages.add("was hit by a love bomb from PLAYER. FINAL KILL!");
         deathMessages.add("was no match for PLAYER.");
+        deathMessages.add("was no match for PLAYER. FINAL KILL!");
         deathMessages.add("was smote from afar by PLAYER.");
+        deathMessages.add("was smote from afar by PLAYER. FINAL KILL!");
         deathMessages.add("was justly ended by PLAYER.");
+        deathMessages.add("was justly ended by PLAYER. FINAL KILL!");
         deathMessages.add("was purified by PLAYER.");
+        deathMessages.add("was purified by PLAYER. FINAL KILL!");
         deathMessages.add("was killed with holy water by PLAYER.");
+        deathMessages.add("was killed with holy water by PLAYER. FINAL KILL!");
         deathMessages.add("was dealt vengeful justice by PLAYER.");
+        deathMessages.add("was dealt vengeful justice by PLAYER. FINAL KILL!");
         deathMessages.add("was returned to dust by PLAYER.");
+        deathMessages.add("was returned to dust by PLAYER. FINAL KILL!");
         deathMessages.add("be shot and killed by PLAYER.");
+        deathMessages.add("be shot and killed by PLAYER. FINAL KILL!");
         deathMessages.add("be snowballed to death by PLAYER.");
+        deathMessages.add("be snowballed to death by PLAYER. FINAL KILL!");
         deathMessages.add("be sent to Davy Jones' locker by PLAYER.");
+        deathMessages.add("be sent to Davy Jones' locker by PLAYER. FINAL KILL!");
         deathMessages.add("be killed with rum by PLAYER.");
+        deathMessages.add("be killed with rum by PLAYER. FINAL KILL!");
         deathMessages.add("be shot with cannon by PLAYER.");
+        deathMessages.add("be shot with cannon by PLAYER. FINAL KILL!");
         deathMessages.add("be killed with magic by PLAYER.");
+        deathMessages.add("be killed with magic by PLAYER. FINAL KILL!");
         deathMessages.add("was glazed in BBQ sauce by PLAYER.");
+        deathMessages.add("was glazed in BBQ sauce by PLAYER. FINAL KILL!");
         deathMessages.add("was sprinkled with chilli powder by PLAYER.");
+        deathMessages.add("was sprinkled with chilli powder by PLAYER. FINAL KILL!");
         deathMessages.add("was sprinkled with chili powder by PLAYER.");
+        deathMessages.add("was sprinkled with chili powder by PLAYER. FINAL KILL!");
         deathMessages.add("was sliced up by PLAYER.");
+        deathMessages.add("was sliced up by PLAYER. FINAL KILL!");
         deathMessages.add("was overcooked by PLAYER.");
+        deathMessages.add("was overcooked by PLAYER. FINAL KILL!");
         deathMessages.add("was deep fried by PLAYER.");
+        deathMessages.add("was deep fried by PLAYER. FINAL KILL!");
         deathMessages.add("was boiled by PLAYER.");
+        deathMessages.add("was boiled by PLAYER. FINAL KILL!");
         deathMessages.add("was spooked by PLAYER.");
+        deathMessages.add("was spooked by PLAYER. FINAL KILL!");
         deathMessages.add("was spooked off the map by PLAYER.");
+        deathMessages.add("was spooked off the map by PLAYER. FINAL KILL!");
         deathMessages.add("was totally spooked by PLAYER.");
+        deathMessages.add("was totally spooked by PLAYER. FINAL KILL!");
         deathMessages.add("was remotely spooked by PLAYER.");
+        deathMessages.add("was remotely spooked by PLAYER. FINAL KILL!");
     }
 
     public void storeNick(String nick) {
